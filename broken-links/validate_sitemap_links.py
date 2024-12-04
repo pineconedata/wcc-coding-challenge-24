85536a631caea2dc89ae5fc35aec54099ab6ca22
pineconedata <emily@pineconedata.com>
2024-12-03 19:31:21 -0500

    add function to parse sitemap content

--- broken-links/validate_sitemap_links.py
+++ broken-links/validate_sitemap_links.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_sitemap(sitemap_url):
@@ -12,10 +13,23 @@ def get_sitemap(sitemap_url):
         print(f'Error: The request to {sitemap_url} timed out.')
         return None
     except requests.exceptions.RequestException as e:
-        print(f'Error: Failed to fetch sitemap from {sitemap_url}. Detailed error information: {e}')
+        print(f'Error: Failed to fetch sitemap from {sitemap_url}. Details: {e}')
         return None
+
+
+def parse_sitemap(sitemap_content):
+    try:
+        print('Parsing sitemap content.')
+        soup = BeautifulSoup(sitemap_content, 'xml')
+        urls = [loc.text for loc in soup.find_all('loc')]
+        print(f'Parsed sitemap content. Found {len(urls)} URLs.')
+        return urls
+    except Exception as e:
+        print(f'Error: Failed to parse the sitemap XML. Details: {e}')
+        return []
 
 
 sitemap_url = 'https://www.pineconedata.com/sitemap.xml'
 sitemap_timeout = 10
-get_sitemap(sitemap_url)
+sitemap_content = get_sitemap(sitemap_url)
+parse_sitemap(sitemap_content)

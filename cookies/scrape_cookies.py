5bc56d3097e516b35b5f49c0ac8e66c1f34f7815
pineconedata <emily@pineconedata.com>
2024-12-23 18:08:04 -0500

    add support for chrome browser and browser_type parameter

--- cookies/scrape_cookies.py
+++ cookies/scrape_cookies.py
@@ -3,19 +3,33 @@ import pandas
 import logging
 import selenium
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver import FirefoxOptions, ChromeOptions
+from selenium.webdriver.chrome.service import Service
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
 
-def setup_driver():
-    """Setup the webdriver with necessary options."""
+def setup_driver(browser_type):
+    """Setup the webdriver with necessary options for given browser type."""
     try:
-        logging.info('Setting up the selenium WebDriver...')
-        options = FirefoxOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Firefox(options=options)
+        logging.info(f'Setting up the selenium WebDriver for {browser_type}...')
+
+        if browser_type.lower() == 'firefox':
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            driver = webdriver.Firefox(options=options)
+
+        elif browser_type.lower() == 'chrome':
+            service = Service('/usr/bin/chromedriver')
+            options = ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            driver = webdriver.Chrome(options=options, service=service)
+
+        else:
+            raise ValueError(f'Unsupported browser type: {browser_type}')
+
         return driver
     except selenium.common.exceptions.SessionNotCreatedException as e:
         logging.error(f'Unable to create WebDriver session. Details: {e}')
@@ -33,6 +47,7 @@ def get_cookies(driver):
         logging.info('Getting cookies...')
         cookies = driver.get_cookies()
         cookies_df = pandas.DataFrame(cookies)
+        logging.info(f'Cookies found: {len(cookies)}')
         return cookies_df
     except selenium.common.exceptions.TimeoutException as e:
         logging.error(f"Timeout while trying to retrieve cookies: {e}")
@@ -45,8 +60,7 @@ def get_cookies(driver):
 def export_cookies(writer, df, sheet_name):
     """Export the DataFrame to an Excel file in the specified sheet."""
     try:
-        logging.info(f'Exporting cookies for {sheet_name}')
-        print(df)
+        logging.info('Exporting cookies...')
         df.to_excel(writer, sheet_name=sheet_name, index=None)
         writer.close
     except Exception as e:
@@ -54,16 +68,16 @@ def export_cookies(writer, df, sheet_name):
 
 
 if __name__ == "__main__":
-    export_file = 'cookies_data.xlsx'
+    browser_type = 'chrome'
+    export_file = f'cookies_data_{browser_type}.xlsx'
 
-    driver = setup_driver()
+    driver = setup_driver(browser_type)
 
     with pandas.ExcelWriter(export_file, engine='xlsxwriter') as writer:
-        homepage_url = 'https://www.pineconedata.com/'
-        logging.info(f'Getting cookies for {homepage_url}')
+        homepage_url = 'https://www.vrbo.com/'
         driver.get(homepage_url)
-        driver.add_cookie({"name": "test", "value": "cookie"})
         cookies = get_cookies(driver)
         export_cookies(writer, cookies, 'homepage')
 
     driver.quit
+    logging.info(f'Cookies exported to: {export_file}')

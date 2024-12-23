2a41e8b071156ada3957fe96a433a98523d4ed8d
pineconedata <emily@pineconedata.com>
2024-12-22 23:21:00 -0500

    add function to get cookies using the webdriver

--- cookies/scrape_cookies.py
+++ cookies/scrape_cookies.py
@@ -1,4 +1,5 @@
 import sys
+import pandas
 import logging
 import selenium
 from selenium import webdriver
@@ -25,9 +26,28 @@ def setup_driver():
         logging.error(f'Encountered error setting up WebDriver. Details: {e}')
 
 
+def get_cookies(driver):
+    try:
+        cookies = driver.get_cookies()
+        cookies_df = pandas.DataFrame(cookies)
+        return cookies_df
+    except selenium.common.exceptions.TimeoutException as e:
+        logging.error(f"Timeout while trying to retrieve cookies: {e}")
+    except selenium.common.exceptions.WebDriverException as e:
+        logging.error(f'WebDriver error encountered while getting cookies. Details: {e}')
+    except Exception as e:
+        logging.error(f'Encountered error getting cookies. Details: {e}')
+
+
 if __name__ == "__main__":
+    homepage_url = 'https://www.pineconedata.com/'
+
     driver = setup_driver()
-    driver.get('https://www.pineconedata.com/')
-    title = driver.title
-    logging.info(title)
+
+    logging.info(f'Getting cookies for {homepage_url}')
+    driver.get(homepage_url)
+    driver.add_cookie({"name": "test", "value": "cookie"})
+    cookies = get_cookies(driver)
+    print(cookies)
+
     driver.quit

7f0073323bbb06ad952e2a1fe1340c7de83d1cd3
pineconedata <emily@pineconedata.com>
2024-12-28 23:33:13 -0500

    refactor get_cookies_db and get_cookies to use profile_dir

--- cookies/scrape_cookies.py
+++ cookies/scrape_cookies.py
@@ -24,6 +24,7 @@ def setup_driver(browser_type, headless=False):
             if headless:
                 options.add_argument('--headless')
             driver = webdriver.Firefox(options=options)
+            profile_dir = driver.capabilities['moz:profile']
 
         elif browser_type.lower() == 'chrome':
             service = Service('/usr/bin/chromedriver')
@@ -34,12 +35,13 @@ def setup_driver(browser_type, headless=False):
             tempdir = tempfile.mkdtemp(dir=os.getcwd())
             options.add_argument(f'--user-data-dir={tempdir}')
             driver = webdriver.Chrome(options=options, service=service)
+            profile_dir = driver.capabilities['chrome']['userDataDir']
 
         else:
             raise ValueError(f'Unsupported browser type: {browser_type}')
             sys.exit(1)
 
-        return driver
+        return driver, profile_dir
     except selenium.common.exceptions.SessionNotCreatedException as e:
         logging.error(f'Unable to create WebDriver session. Details: {e}')
         sys.exit(1)
@@ -51,13 +53,13 @@ def setup_driver(browser_type, headless=False):
         sys.exit(1)
 
 
-def get_cookies(driver, browser_type, cookie_method):
+def get_cookies(driver, browser_type, cookie_method, profile_dir):
     """Call the appropriate get_cookies_ function based on the given cookie_method."""
     logging.info(f'Getting cookies from the {cookie_method}...')
     if cookie_method.lower() == 'webdriver':
         cookies_df = get_cookies_wd(driver)
     elif cookie_method.lower() == 'database':
-        cookies_df = get_cookies_db(browser_type)
+        cookies_df = get_cookies_db(browser_type, profile_dir)
     else:
         raise ValueError(f'Unsupported cookie method: {cookie_method}')
         sys.exit(1)
@@ -79,17 +81,15 @@ def get_cookies_wd(driver):
         logging.error(f'Encountered error getting cookies. Details: {e}')
 
 
-def get_cookies_db(browser_type):
+def get_cookies_db(browser_type, profile_dir):
     """Get cookies from the given browser's cookie database and return the data in a DataFrame."""
     try:
         if browser_type.lower() == 'firefox':
             query = 'SELECT * FROM moz_cookies'
-            profile_dir = driver.capabilities['moz:profile']
             database_path = f'{profile_dir}/cookies.sqlite'
             temp_db = f'{os.getcwd()}/cookies.sqlite'
         elif browser_type.lower() == 'chrome':
             query = 'SELECT * FROM cookies'
-            profile_dir = driver.capabilities['chrome']['userDataDir']
             database_path = f'{profile_dir}/Default/Cookies'
             temp_db = f'{os.getcwd()}/Cookies'
 
@@ -112,7 +112,7 @@ def get_cookies_db(browser_type):
 
 
 def export_cookies(df, excel_writer, **kwargs):
-    """Export the DataFrame to an Excel file in the specified sheet."""
+    """Export the DataFrame to an Excel file."""
     try:
         logging.info('Exporting cookies...')
         df.to_excel(excel_writer, **kwargs)
@@ -156,10 +156,10 @@ if __name__ == "__main__":
     url = 'https://www.pineconedata.com/'
     export_file = f'cookies_data_{browser_type}_{cookie_method}.xlsx'
 
-    driver = setup_driver(browser_type, headless)
+    driver, profile_dir = setup_driver(browser_type, headless)
     driver.get(url)
     driver = add_sample_cookies(driver)
-    cookies = get_cookies(driver, browser_type, cookie_method)
+    cookies = get_cookies(driver, browser_type, cookie_method, profile_dir)
     export_cookies(cookies, export_file, index=False)
 
     driver.quit()

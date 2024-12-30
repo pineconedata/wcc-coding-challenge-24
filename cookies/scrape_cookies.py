e9202cb3b45fd3e1d2c287d48ac6e364455ed693
pineconedata <emily@pineconedata.com>
2024-12-29 21:54:11 -0500

    add cleanup function and improve error handling

--- cookies/scrape_cookies.py
+++ cookies/scrape_cookies.py
@@ -20,7 +20,7 @@ def win_to_unix_epoch(win_epoch):
     MICROSECONDS_IN_SECOND = 1000000
 
     if not isinstance(win_epoch, (int, float)):
-        raise ValueError("win_epoch must be an integer or float.")
+        raise ValueError('win_epoch must be an integer or float.')
 
     unix_epoch = (win_epoch / MICROSECONDS_IN_SECOND) - WIN_EPOCH_TO_UNIX_OFFSET
     return unix_epoch
@@ -51,30 +51,28 @@ def setup_driver(browser_type, headless=False):
 
         else:
             raise ValueError(f'Unsupported browser type: {browser_type}')
-            sys.exit(1)
 
         return driver, profile_dir
     except selenium.common.exceptions.SessionNotCreatedException as e:
         logging.error(f'Unable to create WebDriver session. Details: {e}')
-        sys.exit(1)
+        raise
     except selenium.common.exceptions.WebDriverException as e:
         logging.error(f'Issue with the WebDriver. Details: {e}')
-        sys.exit(1)
+        raise
     except Exception as e:
         logging.error(f'Encountered error setting up WebDriver. Details: {e}')
-        sys.exit(1)
+        raise
 
 
 def get_cookies(driver, browser_type, cookie_method, profile_dir):
     """Call the appropriate get_cookies_ function based on the given cookie_method."""
-    logging.info(f'Getting cookies from the {cookie_method}...')
+    logging.info(f'Getting cookies using the {cookie_method} method...')
     if cookie_method.lower() == 'webdriver':
         cookies_df = get_cookies_wd(driver)
     elif cookie_method.lower() == 'database':
         cookies_df = get_cookies_db(browser_type, profile_dir)
     else:
         raise ValueError(f'Unsupported cookie method: {cookie_method}')
-        sys.exit(1)
     logging.info(f'Cookies found: {len(cookies_df)}')
     return cookies_df
 
@@ -83,14 +81,14 @@ def get_cookies_wd(driver):
     """Get cookies from the given WebDriver and return the data in a DataFrame."""
     try:
         cookies = driver.get_cookies()
-        cookies_df = pd.DataFrame(cookies)
-        return cookies_df
+        return pd.DataFrame(cookies)
     except selenium.common.exceptions.TimeoutException as e:
         logging.error(f'Timeout while trying to retrieve cookies: {e}')
     except selenium.common.exceptions.WebDriverException as e:
         logging.error(f'WebDriver error encountered while getting cookies. Details: {e}')
     except Exception as e:
-        logging.error(f'Encountered error getting cookies. Details: {e}')
+        logging.error(f'Encountered error getting cookies from webdriver. Details: {e}')
+        raise
 
 
 def get_cookies_db(browser_type, profile_dir):
@@ -104,10 +102,11 @@ def get_cookies_db(browser_type, profile_dir):
             query = 'SELECT * FROM cookies'
             database_path = f'{profile_dir}/Default/Cookies'
             temp_db = f'{os.getcwd()}/Cookies'
+        else:
+            raise ValueError(f'Unsupported browser: {browser_type}')
 
         if not os.path.exists(database_path):
             raise ValueError(f'Cookies database not found at: {database_path}')
-            sys.exit(1)
 
         shutil.copy(database_path, temp_db)
         conn = sqlite3.connect(temp_db)
@@ -117,14 +116,14 @@ def get_cookies_db(browser_type, profile_dir):
         cols = [description[0] for description in cursor.description]
         os.remove(temp_db)
 
-        cookies_df = pd.DataFrame(data=rows, columns=cols)
-        return cookies_df
+        return pd.DataFrame(data=rows, columns=cols)
     except Exception as e:
-        logging.error(f'Encountered error getting cookies. Details: {e}')
+        logging.error(f'Encountered error getting cookies from database. Details: {e}')
+        raise
 
 
 def format_cookies(df):
-    """Apply formatting changes to cookies dataframe amd return the formatted df."""
+    """Format given cookies dataframe."""
     try:
         logging.info('Formatting cookies...')
         bool_cols = ['is_secure', 'is_httponly', 'has_expires', 'is_persistent', 'has_cross_site_ancestor']
@@ -139,6 +138,7 @@ def format_cookies(df):
         return df
     except Exception as e:
         logging.error(f'Encountered error formatting cookies. Details: {e}')
+        raise
 
 
 def export_cookies(df, excel_writer, **kwargs):
@@ -149,6 +149,7 @@ def export_cookies(df, excel_writer, **kwargs):
         logging.info(f'Cookies exported to: {excel_writer}')
     except Exception as e:
         logging.error(f'Encountered error exporting cookies to Excel. Details: {e}')
+        raise
 
 
 def add_sample_cookies(driver):
@@ -179,25 +180,53 @@ def add_sample_cookies(driver):
     return driver
 
 
+def cleanup(driver=None, profile_dir=None):
+    """Clean up resources including the webdriver and associated profile directory."""
+    if driver:
+        try:
+            if driver.service.is_connectable():
+                driver.quit()
+                logging.info('Cleaned up webdriver.')
+            else:
+                logging.info('Cleaned up webdriver (previously quit or session is no longer active).')
+        except selenium.common.exceptions.WebDriverException as e:
+            logging.error(f'WebDriverException encountered when quitting the webdriver. Details: {e}')
+        except Exception as e:
+            logging.error(f'Error while quitting the driver. Details: {e}')
+
+    if profile_dir and os.path.exists(profile_dir):
+        try:
+            shutil.rmtree(profile_dir)
+            logging.info(f'Cleaned up profile directory at {profile_dir}')
+        except Exception as e:
+            logging.error(f'Error while removing profile directory. Details: {e}')
+
+
 if __name__ == "__main__":
-    headless = True
-    browser_type = 'chrome'
-    cookie_method = 'database'
-    url = 'https://www.pineconedata.com/'
-    export_file = f'cookies_data_{browser_type}_{cookie_method}.xlsx'
+    driver = None
+    profile_dir = None
 
-    driver, profile_dir = setup_driver(browser_type, headless)
-    driver.get(url)
-    driver = add_sample_cookies(driver)
+    try:
+        headless = True
+        browser_type = 'firefox'
+        cookie_method = 'database'
+        url = 'https://www.pineconedata.com/'
+        export_file = f'cookies_data_{browser_type}_{cookie_method}.xlsx'
 
-    if cookie_method.lower() == 'webdriver':
-        cookies = get_cookies(driver, browser_type, cookie_method, profile_dir)
-        export_cookies(cookies, export_file, index=False)
+        driver, profile_dir = setup_driver(browser_type, headless)
+        driver.get(url)
+        driver = add_sample_cookies(driver)
 
-    driver.quit()
-    if cookie_method.lower() == 'database':
-        cookies = get_cookies(driver, browser_type, cookie_method, profile_dir)
-        cookies = format_cookies(cookies)
-        export_cookies(cookies, export_file, index=False)
+        if cookie_method.lower() == 'webdriver':
+            cookies = get_cookies(driver, browser_type, cookie_method, profile_dir)
+            export_cookies(cookies, export_file, index=False)
 
-    shutil.rmtree(profile_dir)
+        driver.quit()
+        if cookie_method.lower() == 'database':
+            cookies = get_cookies(driver, browser_type, cookie_method, profile_dir)
+            cookies = format_cookies(cookies)
+            export_cookies(cookies, export_file, index=False)
+    except Exception as e:
+        logging.error(f'Error: {e}')
+    finally:
+        cleanup(driver, profile_dir)
